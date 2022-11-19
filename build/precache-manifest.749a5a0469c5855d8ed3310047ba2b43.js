7ca64f87db0b9456bfaac233814fe873b00a5cf0
naga venkata sai kommu <knvsai0007@gmail.com>
2022-11-19 21:47:30 +0530

    Add files via upload

---
+++ build/precache-manifest.749a5a0469c5855d8ed3310047ba2b43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a64587e2c92f77ed24f",
+    "url": "/static/css/main.9a22f6d3.chunk.css"
+  },
+  {
+    "revision": "5a64587e2c92f77ed24f",
+    "url": "/static/js/main.5a64587e.chunk.js"
+  },
+  {
+    "revision": "f52d549bf3c3dfce35d8",
+    "url": "/static/js/1.f52d549b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "654f69143cef82d23777ba50f2372237",
+    "url": "/static/media/hero.654f6914.png"
+  },
+  {
+    "revision": "4ce2a94ffb4079016755bbfae08dc815",
+    "url": "/index.html"
+  }
+];
